c59a941d9191ebc3e2dca4a38314c6a0a057ab2e
David Marek <david@marek.me>
2018-05-01 22:57:19 +0200

    Added AlphaVantage models and test
    - metadata part of time series daily call response

---
+++ StocksBot.UnitTests/AlphaVantageModelTests.cs
@@ -0,0 +1,71 @@
+using Newtonsoft.Json;
+using System;
+using Xunit;
+using StocksBot.StocksProviders.Models;
+
+namespace StocksBot.UnitTests
+{
+    public class AlphaVantageModelTests
+    {
+        private const string TimeSeriesDailyResponse = @"{
+            ""Meta Data"": {
+                ""1. Information"": ""Daily Prices (open, high, low, close) and Volumes"",
+                ""2. Symbol"": ""MSFT"",
+                ""3. Last Refreshed"": ""2018-05-01 15:32:55"",
+                ""4. Output Size"": ""Compact"",
+                ""5. Time Zone"": ""US/Eastern""
+            },
+            ""Time Series (Daily)"": {
+                ""2018-05-01"": {
+                    ""1. open"": ""93.2100"",
+                    ""2. high"": ""95.2900"",
+                    ""3. low"": ""92.7900"",
+                    ""4. close"": ""95.0000"",
+                    ""5. volume"": ""31057858""
+                },
+                ""2018-04-30"": {
+                    ""1. open"": ""96.3300"",
+                    ""2. high"": ""96.3964"",
+                    ""3. low"": ""93.1500"",
+                    ""4. close"": ""93.5200"",
+                    ""5. volume"": ""41092435""
+                }
+            }
+        }";
+
+        [Fact]
+        public void AlphaVantageTimeSeriesDailyResponse_Parsed_InformationIsCorrect()
+        {
+            var parsedData = JsonConvert.DeserializeObject<AlphaVantageTimeSeriesDailyResponse>(TimeSeriesDailyResponse);
+            Assert.Equal("Daily Prices (open, high, low, close) and Volumes", parsedData.Metadata.Information);
+        }
+
+        [Fact]
+        public void AlphaVantageTimeSeriesDailyResponse_Parsed_SymbolIsCorrect()
+        {
+            var parsedData = JsonConvert.DeserializeObject<AlphaVantageTimeSeriesDailyResponse>(TimeSeriesDailyResponse);
+            Assert.Equal("MSFT", parsedData.Metadata.Symbol);
+        }
+
+        [Fact]
+        public void AlphaVantageTimeSeriesDailyResponse_Parsed_LastRefreshedIsCorrect()
+        {
+            var parsedData = JsonConvert.DeserializeObject<AlphaVantageTimeSeriesDailyResponse>(TimeSeriesDailyResponse);
+            Assert.Equal(new DateTime(2018, 5, 1, 15, 32, 55), parsedData.Metadata.LastRefreshed);
+        }
+
+        [Fact]
+        public void AlphaVantageTimeSeriesDailyResponse_Parsed_OutputSizedIsCorrect()
+        {
+            var parsedData = JsonConvert.DeserializeObject<AlphaVantageTimeSeriesDailyResponse>(TimeSeriesDailyResponse);
+            Assert.Equal("Compact", parsedData.Metadata.OutputSize);
+        }
+
+        [Fact]
+        public void AlphaVantageTimeSeriesDailyResponse_Parsed_TimeZoneIsCorrect()
+        {
+            var parsedData = JsonConvert.DeserializeObject<AlphaVantageTimeSeriesDailyResponse>(TimeSeriesDailyResponse);
+            Assert.Equal("US/Eastern", parsedData.Metadata.TimeZone);
+        }
+    }
+}
---
+++ StocksBot/StocksProviders/Models/AlphaVantageTimeSeriesDailyResponse.cs
@@ -0,0 +1,10 @@
+﻿using Newtonsoft.Json;
+
+namespace StocksBot.StocksProviders.Models
+{
+    public class AlphaVantageTimeSeriesDailyResponse
+    {
+        [JsonProperty("Meta Data")]
+        public AlphaVantageTimeSeriesDailyMetadata Metadata { get; set; }
+    }
+}
