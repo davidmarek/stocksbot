521db884d479b0547d0ab0eaea28d5b777b1c8c7
David Marek <david@marek.me>
2018-06-09 22:25:09 +0200

    Added logging for update url secret

--- StocksBot/Controllers/TelegramController.cs
+++ StocksBot/Controllers/TelegramController.cs
@@ -2,6 +2,7 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using StocksBot.StocksProviders;
 using StocksBot.Telegram;
@@ -14,12 +15,18 @@ namespace StocksBot.Controllers
     {
         private readonly ITelegramBot telegramBot;
         private readonly IUpdateParser updateParser;
+        private readonly ILogger<TelegramController> logger;
         private readonly TelegramConfiguration configuration;
 
-        public TelegramController(ITelegramBot telegramBot, IUpdateParser updateParser, IOptionsSnapshot<TelegramConfiguration> options)
+        public TelegramController(
+            ITelegramBot telegramBot, 
+            IUpdateParser updateParser, 
+            IOptionsSnapshot<TelegramConfiguration> options,
+            ILogger<TelegramController> logger)
         {
             this.telegramBot = telegramBot;
             this.updateParser = updateParser;
+            this.logger = logger;
             this.configuration = options.Value;
         }
 
@@ -28,7 +35,10 @@ namespace StocksBot.Controllers
         public ActionResult Update(string secret, [FromBody] Update update, CancellationToken cancellationToken)
         {
             if (secret != this.configuration.WebhookSecret)
+            {
+                this.logger.LogWarning("Invalid secret: {0}, expected {1}", secret, this.configuration.WebhookSecret);
                 return this.BadRequest();
+            }
 
             this.updateParser.ProcessUpdateAsync(update, cancellationToken);
             return this.Ok();
